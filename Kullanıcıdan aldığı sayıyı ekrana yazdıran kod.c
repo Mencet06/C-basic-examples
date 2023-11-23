1859c41bc225b100fccabf688698590475a52f03
Mencet06 <151033641+Mencet06@users.noreply.github.com>
2023-11-23 15:18:26 +0300

    Add files via upload
    Operatörler, döngüler, fonksiyonlar, tek ve iki boyutlu diziler,
    dizelerle, dosyalarla, işaretçilerle işlemler gerçekleştirme vb. kod örnekleri eklenecektir.

---
+++ Kullanıcıdan aldığı sayıyı ekrana yazdıran kod.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main()
+{
+    int num; // Num = number yani sayýnýn ingilizcesi olan kelimeyi tanýmladým.
+    printf("Lutfen bir sayi giriniz: ");
+    scanf("%d",&num);
+    printf("Girdiginiz sayi : %d",num);
+    return 0;
+
+}
